6e2a07f3d7cc79fbfe1b6eea12fe2ddb3403e7a9
geekplux <geekplux@qq.com>
2016-09-05 02:06:17 +0800

    base bar chart

--- example/index.js
+++ example/index.js
@@ -1,4 +1,12 @@
 import Bar from '../index'
 
-const bar = new Bar()
-console.log(bar.d3)
+const data = []
+
+for (let i = 0, n = 10; i < n; ++i) {
+  data.push(Math.floor(Math.random() * n))
+}
+
+const bar = new Bar({
+  target: '.container',
+  data: data
+})
--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-import * as d3 from 'd3'
+import { select, scaleLinear, max } from 'd3'
+
+const defaultOptions = {
+  class: 'bar',
+  width: 500,
+  height: 200,
+  barWidth: 50,
+  barPadding: 5
+}
 
 export default class Bar {
-  constructor () {
-    this.d3 = d3
+
+  constructor (options) {
+    this.init(options)
+  }
+
+  init (options) {
+    const config = Object.assign(defaultOptions, options)
+
+    const y = scaleLinear()
+            .domain([0, max(config.data)])
+            .range([0, config.height])
+
+    this.chart = select(config.target)
+
+    const svg = this.chart
+            .append('svg')
+            .attr('class', config.class)
+            .attr('width', config.width)
+            .attr('height', config.height)
+
+    const group = svg
+            .selectAll('g')
+            .data(config.data)
+            .enter()
+            .append('g')
+            .attr('transform', (d, i) => `translate(${i * config.barWidth}, 0)`)
+
+    group
+      .append('rect')
+      .attr('y', d => config.height - y(d))
+      .attr('width', config.barWidth - config.barPadding)
+      .attr('height', y)
+
+    group
+      .append('text')
+      .attr('x', config.barWidth / 2)
+      .attr('y', config.height - 10)
+      .attr('fill', '#fff')
+      .attr('dy', '.35em')
+      .text(d => d)
   }
 }
